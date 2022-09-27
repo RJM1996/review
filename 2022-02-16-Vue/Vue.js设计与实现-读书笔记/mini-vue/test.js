43c15b2947dac52f5bc826622c57e1b71e4db647
rongjunming <rjm96@foxmail.com>
2022-09-27 21:54:43 +0800

    ✨ feat(vue - test):

--- 2022-02-16-Vue/Vue.js设计与实现-读书笔记/mini-vue/test.js
+++ 2022-02-16-Vue/Vue.js设计与实现-读书笔记/mini-vue/test.js
@@ -12,4 +12,42 @@ function test1() {
 
   console.log({ map, weakMap })
 }
-test1()
+// test1()
+
+function test2() {
+  const obj = {
+    val: 0,
+    [Symbol.iterator]() {
+      return {
+        next() {
+          return {
+            value: obj.val++,
+            done: obj.val > 10,
+          }
+        },
+      }
+    },
+  }
+  for (const o of obj) {
+    console.log({ o })
+  }
+
+  const arr = [1, 2, 3, 4, 5]
+  const itr = arr[Symbol.iterator]()
+  console.log(itr.next())
+  console.log(itr.next())
+  console.log(itr.next())
+  console.log(itr.next())
+  console.log(itr.next())
+  console.log(itr.next())
+  console.log(itr.next())
+}
+// test2()
+
+function test3 () {
+  const obj = {}
+  const arr = [obj]
+  console.log({arr: arr.includes(arr[0])})
+}
+test3()
+
